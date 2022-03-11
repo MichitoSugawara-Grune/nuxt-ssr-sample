0e82ac343539b01c796f9bf6f4c3d7ea7b30aaa9
MichitoSugawara-Grune <82135459+MichitoSugawara-Grune@users.noreply.github.com>
2022-03-11 16:54:30 +0900

    initial commit

---
+++ pages/postcode/_postcode.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <h2>{{ response }}</h2>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'axios',
+  data() {
+    return {
+      response: '',
+    }
+  },
+  async asyncData({ $axios, params }) {
+    const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec))
+    // ipアドレスを取得
+    const response = await $axios.$get(
+      'https://zipcloud.ibsnet.co.jp/api/search?zipcode=' + params.postcode
+    )
+    // 5秒待つ
+    await sleep(5000)
+    return { response }
+  },
+}
+</script>
